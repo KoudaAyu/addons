948c7dcc5b0cffa592ff3c401b48064224f6a43c
AyuKouda <k024g0061@g.neec.ac.jp>
2026-06-19 16:03:16 +0900

    Menuの完成

--- level_editor.py
+++ level_editor.py
@@ -4,8 +4,8 @@ import bpy
 bl_info = {
     "name": "レベルエディタ",
     "author": "Kouda Ayu",
-    "version": (1,0),
-    "blender": (3,3,1),
+    "version": (1, 0),
+    "blender": (3, 3, 1),
     "location": "",
     "description": "レベルエディタ",
     "warning": "",
@@ -14,13 +14,66 @@ bl_info = {
     "category": "Object"
 }
 
+# メニュー項目描画
+def draw_menu_manual(self, context):
+    # self : 呼び出し元のクラスインスタンス
+    # context : カーソルを合わせたときのポップアップのカスタマイズなどに使用
+    
+    # トップバーの「ヘルプメニュー」に項目(オペレータ)を追加
+    self.layout.operator("wm.url_open_preset", text="Manual", icon="HELP")
+
+# トップバーの拡張メニュー
+class TOPBAR_MT_my_menu(bpy.types.Menu):
+    #Blenderがクラスを認識する為の固有の文字列
+    bl_idname = "TOPBAR_MT_my_menu"
+    #メニューのラベルとして表示される文字列
+    bl_label = "Mymenu"
+    #著者表示用の文字列
+    bl_description = "拡張メニュー by " + bl_info["author"]
+
+    # サブメニューの描画
+    def draw(self,context):
+
+        #トップバーの「エディターメニュー」に項目(オペレータ)を追加
+        self.layout.operator("wm.url_open_preset", text="Manual", icon="HELP")
+
+    # 既存のメニューにサブメニューを追加
+    def submenu(self,context):
+
+        # ID指定でサブメニューを追加
+        self.layout.menu(TOPBAR_MT_my_menu.bl_idname)
+
+# Blenderに登録するクラスリスト
+classes = (
+    TOPBAR_MT_my_menu,
+)
+
+
 # アドオン有効化時コールバック
 def register():
+    # Blenderにクラスを登録
+    for cls in classes:
+        bpy.utils.register_class(cls)
+
+    # 「TOPBAR_MT_editor_menus」に変更して、トップバーに直接追加する
+    bpy.types.TOPBAR_MT_editor_menus.append(TOPBAR_MT_my_menu.submenu)
     print("レベルエディタが有効化されました。")
 
 # アドオン無効化時コールバック
 def unregister():
+    # 削除側も同じに合わせる
+    # メニューから項目を削除
+    bpy.types.TOPBAR_MT_editor_menus.remove(TOPBAR_MT_my_menu.submenu)
+    
+    # Blenderからクラスを削除
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+    
     print("レベルエディタが無効化されました。")
+
+    
+
+
 
 # テスト実行用コード
 if __name__ == "__main__":
